bf0bd4609f035076dd9bb2b38fb234d9150af578
NOUNI EL BACHIR <nouni.elbachir@gmail.com>
2017-03-04 12:52:59 +0000

    Add service provider

---
+++ src/Providers/BaseServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elbachirnouni
+ * Date: 04/03/2017
+ * Time: 12:52
+ */
+
+namespace Enimiste\LaravelWebApp\Core\Providers;
+
+
+use Illuminate\Support\ServiceProvider;
+
+abstract class BaseServiceProvider extends ServiceProvider
+{
+
+}
---
+++ src/Providers/WebAppServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elbachirnouni
+ * Date: 04/03/2017
+ * Time: 12:52
+ */
+
+namespace Enimiste\LaravelWebApp\Core\Providers;
+
+
+class WebAppServiceProvider extends BaseServiceProvider
+{
+
+}
